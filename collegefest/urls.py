b014ce24bbd0a67f8d608c9465354944eb7907b6
Your Name <you@example.com>
2025-09-16 21:28:47 +0530

    Initial

--- collegefest/urls.py
+++ collegefest/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to College Fest!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
